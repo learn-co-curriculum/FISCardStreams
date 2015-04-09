3c7d06924786805beaaa9ba65f1ec5b854694155
github username <github email address>
2015-04-09 11:55:46 -0400

    Added TableView but nothing done yet

---
+++ FISCardStreams/FISCardStreams/TestingWebViewTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  TestingWebViewTableViewCell.h
+//  FISCardStreams
+//
+//  Created by Anish Kumar on 4/9/15.
+//  Copyright (c) 2015 Mark Edward Murray. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestingWebViewTableViewCell : UITableViewCell
+
+@end
---
+++ FISCardStreams/FISCardStreams/TestingWebViewTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  TestingWebViewTableViewCell.m
+//  FISCardStreams
+//
+//  Created by Anish Kumar on 4/9/15.
+//  Copyright (c) 2015 Mark Edward Murray. All rights reserved.
+//
+
+#import "TestingWebViewTableViewCell.h"
+
+@implementation TestingWebViewTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
